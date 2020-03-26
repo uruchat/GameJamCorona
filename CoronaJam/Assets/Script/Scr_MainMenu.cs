48a9f141884d9b914fd16facf6b92bbd61e31912
Azanen <ld.philippe.beauchemin@gmail.com>
2020-03-26 19:31:40 -0400

    Add Basic Project

---
+++ CoronaJam/Assets/Script/Scr_MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // Scenes Loading
+using UnityEngine.UI; // For using UI
+using TMPro; // For using Text Mesh Pro
+
+public class Scr_MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
