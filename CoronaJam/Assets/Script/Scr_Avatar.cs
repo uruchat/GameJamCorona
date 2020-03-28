05f24745d33a8d1f977c744be955e7f0968e524b
Banting43 <62770197+Banting43@users.noreply.github.com>
2020-03-27 21:22:04 -0400

    Avatar Move

---
+++ CoronaJam/Assets/Script/Scr_Avatar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_Avatar : MonoBehaviour
+{
+    private Transform tr;
+    //private int Speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = this.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tr.Translate(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f) * Time.deltaTime * 10f, Space.World);
+    }
+}
