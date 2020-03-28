59856e5a2a64c106552dfdd9f76a70602b4671c5
Azanen <ld.philippe.beauchemin@gmail.com>
2020-03-27 22:47:06 -0400

    scr camera et scene

--- CoronaJam/Assets/Script/Scr_Camera.cs
+++ CoronaJam/Assets/Script/Scr_Camera.cs
@@ -7,9 +7,20 @@ public class Scr_Camera : MonoBehaviour
     private Vector3 Comptoir = new Vector3(0, 0, -10);
     private Vector3 Entrepot = new Vector3(0, 10.8f, -10);
 
+
     public void TransitionCamera()
     {
-        this.transform.position = this.transform.position == Comptoir ? Entrepot : Comptoir;
+        // this.transform.position = this.transform.position == Comptoir ? Entrepot : Comptoir;
+
+        if (this.transform.position == Comptoir)
+        {
+            this.transform.position = Vector3.Lerp(Comptoir, Entrepot, 1);
+
+        }
+        else
+        {
+            this.transform.position = Vector3.Lerp(Entrepot, Comptoir, 1);
+        }
     }
 
 
