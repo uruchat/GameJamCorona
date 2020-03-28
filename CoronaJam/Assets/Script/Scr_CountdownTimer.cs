8503a2c455ed28ca1461a5e428b9d84aaf525ad8
Azanen <ld.philippe.beauchemin@gmail.com>
2020-03-27 22:59:07 -0400

    Revert "Revert "Merge branch 'master' of https://github.com/Azanen/GameJamCorona""
    This reverts commit 7a9194eeafedd8e1369beb819b75678d4c265a69.

---
+++ CoronaJam/Assets/Script/Scr_CountdownTimer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scr_CountdownTimer : MonoBehaviour
+{
+    private float currentTime = 0f;
+    private float startingTime = 25f; //duree en seconde du Timer
+
+    public Text countdownText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0"); 
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0; //si timer descent en bas de 0, il va rester a 0. Fonction a ajouter pour redemarrer lvl ou menu
+        }
+    }
+}
---
+++ CoronaJam/Assets/Script/Scr_ItemManagement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_ItemManagement : MonoBehaviour
+{
+    public Scr_Manager manager;
+    public bool pickUpAllowed = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pickUpAllowed && Input.GetKeyDown(KeyCode.E))
+        {
+            PickUp();
+        }
+
+    }
+    // Fonctions
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Avatar"))
+        {
+            pickUpAllowed = true;
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        pickUpAllowed = false;
+    }
+    private void PickUp()
+    {
+        if (this.CompareTag("PotionShelf") && manager.total < 5)
+        {
+            manager.potionNbr++;
+            manager.total++;
+        }
+        if (this.CompareTag("MaskShelf") && manager.total < 5)
+        {
+            manager.maskNbr++;
+            manager.total++;
+        }
+    }
+}
