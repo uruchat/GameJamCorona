cfa34551c3d440de5a7f59e5882647a38f3e81d2
Azanen <ld.philippe.beauchemin@gmail.com>
2020-03-28 13:12:58 -0400

    Door

---
+++ CoronaJam/Assets/Script/Scr_Door.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_Door : MonoBehaviour
+{
+    public Camera MainCamera;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Avatar"))
+        {
+            MainCamera.GetComponent<Scr_Camera>().TransitionCamera();
+        }
+    }
+
+}
