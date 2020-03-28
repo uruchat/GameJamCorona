c033a0ff562c98c0f9a3d01e719ab5508c9c9ac5
Azanen <ld.philippe.beauchemin@gmail.com>
2020-03-27 21:53:25 -0400

    Ajout pleins de chose

---
+++ CoronaJam/Assets/Script/Scr_Camera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_Camera : MonoBehaviour
+{
+    public Camera MainCamera;
+
+    private Vector3 Comptoir = new Vector3(0, 0, 0);
+    private Vector3 Entrepot = new Vector3(0, 10.8f, 0);
+
+    public void TransitionOfRoom()
+    {
+        MainCamera.transform.position = MainCamera.transform.position == Comptoir ? Entrepot : Comptoir;
+    }
+
+
+
+}
