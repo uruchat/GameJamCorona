a9910b8818f33e2dcb248f811cda73ea5c30eb0f
Banting43 <62770197+Banting43@users.noreply.github.com>
2020-03-28 10:53:57 -0400

    Ajout de systeme de points (work in progress)

--- CoronaJam/Assets/Script/Scr_NPC.cs
+++ CoronaJam/Assets/Script/Scr_NPC.cs
@@ -5,11 +5,17 @@ using UnityEngine.UI;
 
 public class Scr_NPC : MonoBehaviour
 {
-    public int produit_1 = 0; //papier Q
-    public int produit_2 = 0; //bouffe
+    public int produitBesoin_1 = 0; //papier Q qu'il VEUT avoir
+    public int produitBesoin_2 = 0; //bouffe qu'il VEUT avoir
     private int nbrProduitMax = 5;
 
+    public int currentProduit_1 = 0; //papier Q qu'il a en main
+    public int currentProduit_2 = 0; //bouffe qu'il a en main
+
+
     public Text NPCNeed;
+
+    public float satisfactionPoint = 100f;
 
     // Start is called before the first frame update
     void Start()
@@ -21,13 +27,22 @@ public class Scr_NPC : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      //NPCNeed.text
+        //UI des Besoins et Invetaire du NPC
+        //NPCNeed.text = ("Papier Q : {0} / {1} n/Bouffe : {2} / {3}", currentProduit_1, produitBesoin_1, currentProduit_2, produitBesoin_2);
+        //probleme de String a Int  
+
+        //Satisfaction du NPC. point qui descend avec le temps.
+
+        satisfactionPoint -= 0.5f * Time.deltaTime;
+
+        if (currentProduit_1 == produitBesoin_1 && currentProduit_2 == produitBesoin_2) { }
+           
     }
 
     void NeedCreator() //function pour creer le Randomizer de Besoin
     {
-        produit_1 = Random.Range(0, nbrProduitMax);
-        produit_2 = Random.Range(0, nbrProduitMax);
+        produitBesoin_1 = Random.Range(0, nbrProduitMax);
+        produitBesoin_2 = Random.Range(0, nbrProduitMax);
         
     }
 
