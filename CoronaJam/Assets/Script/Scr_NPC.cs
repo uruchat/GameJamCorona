df6f42fc903221bbf3d11f816abf154396cc2d39
Banting43 <62770197+Banting43@users.noreply.github.com>
2020-03-28 11:49:46 -0400

    Update Scr_NPC.cs

--- CoronaJam/Assets/Script/Scr_NPC.cs
+++ CoronaJam/Assets/Script/Scr_NPC.cs
@@ -7,12 +7,11 @@ public class Scr_NPC : MonoBehaviour
 {
     private Transform tr;
 
-    public int potionBesoin_1 = 0; //potion qu'il VEUT avoir
-    public int maskBesoin_2 = 0; //mask qu'il VEUT avoir
-    private int nbrProduitMax = 5;
+    
 
-    public int currentPotion_1 = 0; //papier Q qu'il a en main
-    public int currentMask_2 = 0; //bouffe qu'il a en main
+    public int produit_1 = 0; //potion qu'il VEUT avoir
+    public int produit_2 = 0; //mask qu'il VEUT avoir
+    private int nbrProduitMax = 5 + 1;
 
 
     public Text NPCNeed;
@@ -32,17 +31,15 @@ public class Scr_NPC : MonoBehaviour
     void Update()
     {
         //UI des Besoins et Invetaire du NPC
-        //NPCNeed.text = ("Papier Q : {0} / {1} n/Bouffe : {2} / {3}", currentProduit_1, produitBesoin_1, currentProduit_2, produitBesoin_2);
+        //NPCNeed.text = ("Potion : {0} / {1} n/Mask : {2} / {3}", currentProduit_1, produitBesoin_1, currentProduit_2, produitBesoin_2);
         //probleme de String a Int  
 
-        //Satisfaction du NPC. Point qui descend avec le temps.
-
+        //Satisfaction du NPC. Point qui descend avec le temps
         satisfactionPoint -= 0.5f * Time.deltaTime; //ajustement si necessaire
 
-        if (currentPotion_1 == potionBesoin_1 && currentMask_2 == maskBesoin_2) 
+        if (Produit_1 == 0 && Produit_2 == 0) 
         { 
             score = satisfactionPoint; 
-            if (tr.position.x >= 
         }
         //doit sortir de l'ecran et etre detruit
 
@@ -57,8 +54,8 @@ public class Scr_NPC : MonoBehaviour
     //Mes Fonctions
     void NeedCreator() //function pour creer le Randomizer de Besoin
     {
-        produitBesoin_1 = Random.Range(0, nbrProduitMax);
-        produitBesoin_2 = Random.Range(0, nbrProduitMax);
+        produit_1 = Random.Range(0, nbrProduitMax);
+        produit_2 = Random.Range(0, nbrProduitMax);
         
     }
 
