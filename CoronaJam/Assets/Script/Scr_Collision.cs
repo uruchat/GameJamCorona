da4cf98a1248bd2bef87ea08ca1357b41778f328
Yutso50 <62724166+Yutso50@users.noreply.github.com>
2020-03-29 12:14:15 -0400

    SetupWP

---
+++ CoronaJam/Assets/Script/Scr_Collision.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_Collision : MonoBehaviour
+{
+    public Scr_Avatar avatar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        avatar.canMove = true;
+        
+    }
+    // Fonctions
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Avatar"))
+        {
+            avatar.canMove = false;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Avatar"))
+        {
+            avatar.canMove = true;
+        }
+    }
+}
