43f9e6051148153f1a3838b34719b6cc2acfba70
Azanen <ld.philippe.beauchemin@gmail.com>
2020-03-29 12:16:55 -0400

    Menu

---
+++ CoronaJam/Assets/Script/Scr_MenuManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_MenuManager : MonoBehaviour
+{
+    public GameObject UnityLogo;
+    public SpriteRenderer spriteUnity;
+    public Color colorUnity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteUnity = UnityLogo.GetComponent<SpriteRenderer>();
+        colorUnity = spriteUnity.color;
+        colorUnity.a -= 0.10f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        colorUnity =  new Vector4(colorUnity.r,colorUnity.g,colorUnity.b,colorUnity.a-0.1f);
+
+    }
+}
